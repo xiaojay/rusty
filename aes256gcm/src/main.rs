d5fce5aed5c9556683601f02e598e2a3ae5b692a
xiaojay <xiaojay@gmail.com>
2020-01-11 23:32:36 +0800

    aes-256-gcm

---
+++ aes256gcm/src/main.rs
@@ -0,0 +1,18 @@
+use ring::aead;
+use base64;
+use rand::{thread_rng, Rng};
+
+fn main() {
+    let mut to_encrypt = String::from("hello world").as_bytes().to_vec();
+    let key = [0; 32];
+    let sealing_key = aead::SealingKey::new(&aead::AES_256_GCM, &key).expect("Failed get key");
+    let nonce: [u8; 12] = thread_rng().gen();
+    let suffix_len = aead::AES_256_GCM.tag_len();
+    for _ in 0..suffix_len {
+        to_encrypt.push(0);
+    };
+    aead::seal_in_place(&sealing_key, &nonce, &[], &mut to_encrypt, suffix_len);
+    println!("{:?}", key);
+    println!("{:?}", nonce);
+    println!("{}", base64::encode(&to_encrypt))
+}
